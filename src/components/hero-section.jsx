2593f8403cfb3181c1ee33e5494c8b9f840a4009
Emmz07 <chincoekun2002@gmail.com>
2023-08-06 15:20:26 +0100

    first commit

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const HeroSection = () => {
   return (
     <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+  <div className="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
     <div id='tainted' className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className="title-font sm:text-5xl text-5xl mb-4 font-large text-gray-900">Find The Best <span>Freelancers </span> 
         <br className="hidden lg:inline-block" />For Your E-Commerce Business
