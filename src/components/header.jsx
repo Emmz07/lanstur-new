43a0fb1a593f51ab8b6cc49290baf78787719b79
Emmz07 <chincoekun2002@gmail.com>
2023-08-07 17:43:19 +0100

    first commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,8 +14,8 @@ const Header = () => {
       <div>
         <button id='head-butt'  className='p-2 text-gray-700 rounded-md outine-none focus:border-400-gray' onClick={() => setNavbar(!navbar)}>
         {navbar ? (
-            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="48px" height="48px"><path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"/></svg>       ):(
-            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="48px" height="48px"><path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="28px" height="28px"><path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"/></svg>       ):(
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="28px" height="28px"><path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"/></svg>
         )}
         </button>
       </div> 
@@ -23,17 +23,17 @@ const Header = () => {
         <div className={navbar ? " block" : "hidden"}>
             <div id='menu-wrapper' >
                 <ul id='menu-nav' >
-                    <li className='pd-6 text-xl text-black py-2'>
-                       Home 
+                    <li id='home' className='pd-6 text-xl text-black py-2'>
+                       SIGN IN 
                     </li>
                     <li className='pd-6 text-xl text-black py-2'>
-                        Explore
+                        Home
                     </li>
                     <li className='pd-6 text-xl text-black py-2'>
                         Find Jobs
                     </li>
                     <li className='pd-6 text-xl text-black py-2'>
-                        Sign in
+                        Explore
                     </li>
                 </ul>
 
